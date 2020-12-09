8c5a7a2c67d2ebb149d4de06204d8aa31bd6224e
Sénéchal Julien <35048771+Teckinfor@users.noreply.github.com>
2020-12-09 16:07:35 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,44 @@
 #include "populate.h"
+#include <stdlib.h>
+#include <string.h>
 
-struct ids_rule
-{
+struct ids_rule{
+        char* action;
+        char* protocol;
+        char* ip_src;
+        char* port_src;
+        char* direction;
+        char* ip_dst;
+        char* port_dst;
+        char* options;
 } typedef Rule;
 
-void rule_matcher(Rule *rules_ds, ETHER_Frame *frame)
-{
+void rule_matcher(Rule *rules_ds, ETHER_Frame *frame){
 }
 
 
-void read_rules(FILE * file, Rule *rules_ds, int count)
-{
+void read_rules(FILE * file, Rule *rules_ds, int count){
+        char rule[MAXLINE];
+        for(int i = 0; i < count; i++){
+                fgets(rule,MAXLINE,file);
+                rules_ds[i].action = strtok(rule," ");
+                rules_ds[i].protocol = strtok(NULL," ");
+                rules_ds[i].ip_src = strtok(NULL," ");
+                rules_ds[i].port_src = strtok(NULL," ");
+                rules_ds[i].direction = strtok(NULL," ");
+                rules_ds[i].ip_dst = strtok(NULL," ");
+                rules_ds[i].port_dst = strtok(NULL," ");
+                rules_ds[i].options = strtok(NULL,")");
 
+                //Remove the first character from the options which is "("
+                for(int j = 0; j < strlen(rules_ds[i].options); j++){
+                        rules_ds[i].options[j] = rules_ds[i].options[j+1];
+                }
+        }
 }
 
 
-void my_packet_handler(
-        u_char *args,
-        const struct pcap_pkthdr *header,
-        const u_char *packet
-)
-
-{
+void my_packet_handler(u_char *args,const struct pcap_pkthdr *header,const u_char *packet){
 
 }
 
@@ -35,6 +52,7 @@ void print_help_menu(){
 
 int main(int argc, char *argv[]) 
 {
+        printf("MySecureIDS is running : \n");
 
         //char *device = argv[1];
         int is_interface = 0;
@@ -73,6 +91,29 @@ int main(int argc, char *argv[])
         }
 
         if(is_interface && !is_help){
+                
+                //Check the number of rules
+                FILE *file = fopen("ids.rules","r");
+                int n_rules = 0;
+                char rule[MAXLINE];
+
+                if(file == NULL){
+                        printf("An error occurred while reading the rules file\n");
+                        exit(1);
+                }
+                while(fgets(rule,MAXLINE,file)!= NULL){
+                        n_rules ++;
+                }
+                printf("%d rules have been found\n",n_rules);
+                fclose(file);
+
+                //Read all rules
+                FILE *rule_file = fopen("ids.rules","r");
+                Rule lst_rules[n_rules];
+                read_rules(rule_file, lst_rules, n_rules);
+                fclose(rule_file);
+                
+                //Listening on the interface
                 printf("Listening on %s...\n", device);
                 char error_buffer[PCAP_ERRBUF_SIZE];
                 pcap_t *handle;
