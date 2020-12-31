ceefcbaa8332a7f86d9c4db359cfa81cb0a11a8e
Sénéchal Julien <35048771+Teckinfor@users.noreply.github.com>
2020-12-31 13:00:38 +0100

    Create protocol.c

---
+++ protocol.c
@@ -0,0 +1,58 @@
+#include "populate.h"
+
+int show_protocol(ETHER_Frame *frame){
+        if(frame->ethernet_type == ARP){
+                return 5;
+                //ARP
+        }
+
+        if(frame->data.protocol_ip == 1){ 
+                const u_char *temp_pointer = frame->data.data.data;
+                if((char)*temp_pointer == 'H'){
+                                temp_pointer++;
+                        if((char)*temp_pointer == 'T'){
+                                temp_pointer++;
+                                if((char)*temp_pointer == 'T'){
+                                        temp_pointer++;
+                                        if((char)*temp_pointer == 'P'){
+                                                return 3;
+                                                //HTTP
+                                
+                                        }
+                                
+                                }
+                        }
+                        
+                }
+                else if((char)*temp_pointer == 'G'){
+                                temp_pointer++;
+                        if((char)*temp_pointer == 'E'){
+                                temp_pointer++;
+                                if((char)*temp_pointer == 'T'){
+                                        temp_pointer++;
+                                        if((char)*temp_pointer == ' '){
+                                                return 3;
+                                                //HTTP
+                                
+                                        }
+                                
+                                }
+                        }
+                        
+                }
+
+                else if(frame->data.data.destination_port == 443 || frame->data.data.source_port == 443){
+                        return 4;
+                        //HTTPS
+                }
+
+                return 1;
+                //TCP
+        }
+
+        if (frame->data.protocol_ip == 2){
+                return 2;
+                //UDP
+        }
+        return 0; //Not implemented
+}
